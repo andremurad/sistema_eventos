0386e788c3f609d71b634e0b0c9c352c058af60a
andremurad <andremurad09@gmail.com>
2025-10-06 17:01:14 -0300

    upload de arquivos

--- index.php
+++ index.php
@@ -12,16 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $erro = "Preencha todos os campos !";
     }
 
+
     // Upload Imagem
     $caminhoImagem = '';
     if (empty($erro) && !empty($_FILES['imagem']['name']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
 
-        $tamMax = 2 * 1024 * 124;
+        $tamMax = 2*1024*1024;
         if ($_FILES['imagem']['size'] > $tamMax) {
             $erro = "Imagem ta grande em !! (Máx. 2mb)";
         } else {
             $nomeOriginal = basename((string)$_FILES['imagem']['name']);
-            $nomeOriginal = preg_replace('/[^A-Za-z0-9_.-', '_', $nomeOriginal);
+            $nomeOriginal = preg_replace('/[^A-Za-z0-9_.-]/', '_', $nomeOriginal);
             $nomeFinal = time() . '_' . $nomeOriginal;
 
             $destino = __DIR__ . '/uploads/' . $nomeFinal;
@@ -32,9 +33,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $erro = "Falha ao salvar a imagem.";
             }
         }
-        if($empty($erro)){
-            echo "deu certooooo";
+        if(empty($erro)){
+            $linha = $titulo ."|".$data."|".$hora."|".$local."|".$descricao."|".$caminhoImagem.PHP_EOL;
+            
+            // Salva os valores
+            file_put_contents(__DIR__."/eventos.txt",$linha, FILE_APPEND | LOCK_EX);
+            
+            echo "<script>alert('Foi Mermão')</script>";
         }
+    }
+}
+// Carregar Lista
+$eventos = [];
+$arquivo = __DIR__ . "/eventos.txt";
+
+//Verifica se o arquivo existe
+if(is_file($arquivo)){
+    //Ler arquivo por linha
+    $linhas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    //Faz o reverso puff.. para mostrar os primeiros sendo mais recentes
+    $linhas = array_reverse($linhas);
+
+    //Percorre cada linha e quebramos por | em diferentes elementos
+    foreach($linhas as $linha){
+        //Explode na |
+        $p = explode('|',$linha);
+        // Monte pedaços de forma organizada
+        $eventos[] = [
+            'titulo' => $p[0],
+            'data' => $p[1],
+            'hora' => $p[2],
+            'local' => $p[3],
+            'descricao' => $p[4],
+            'imagem' => $p[5]
+        ];
     }
 }
 ?>
@@ -60,6 +92,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <section class="grid">
             <div class="card">
                 <h2 class="card_titulo"> Cadastrar Atividade </h2>
+
+                <?php if(!empty($erro)) : ?>
+                    <div class="aviso erro"><?= $erro ?></div>
+                    <?php endif; ?>
 
                 <form class="form" method="post" enctype="multipart/form-data">
 
@@ -95,8 +131,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </label>
                     <button type="submit" class="botao_primario">Salvar</button>
                 </form>
+                </div>
 
-            </div>
+                <div class="card">
+                    <h2 class="card_titulo">Atividades Cadastradas</h2>
+                    <?php if(empty($eventos)): ?>
+                        <p class="muted">Sem Registro ! , Quer ver o que ?</p>
+                        <?php else: ?>
+                            <div class="lista_eventos">
+                                <?php foreach ($eventos as $e):
+                                $titulo = $e['titulo'];
+                                $data = $e['data'];
+                                $hora = $e['hora'];
+                                $local = $e['local'];
+                                $descricao = nl2br($e['descricao']);
+                                $imagem = $e['imagem'];
+                                ?>
+                                <article class="evento">
+                                    <img class="thumb" src="<?= $imagem ?>" alt="Imagem do evento">
+                                    <h3 class="ev_titulo"><?= $titulo ?></h3>
+                                    <p class="ev_meta">
+                                        Data : <?= $data ?> |
+                                        Hora : <?= $hora ?> |
+                                        Local : <?= $local ?>
+                                </p>
+                                <p><?= $descricao ?></p>
+                                </article>
+                                <?php endforeach; ?>
+                            </div>
+                            <?php endif; ?>
+                </div>
         </section>
     </div>
 
